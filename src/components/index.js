874642169c3efbbbe2eec699b646da87b19584bd
Bari-Elwerfally <bariwer@gmail.com>
2026-01-13 20:41:40 +0200

    Integrate sign-in and sign-up components into main application file (index.js)

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,15 @@
+// Import existing components
 import Home from "./container/Home";
 import About from "./container/About";
-import Questions from "./container/Questions"
+import Questions from "./container/Questions";
 import Services from "./container/Service/Services";
 import ServiceDetails from "./container/Service/ServiceDetails";
 import Contact from "./container/Contact";
 import Footer from "./container/Footer";
 import Navbar from "./Navbar/Navbar";
 
-export { Navbar, Home, About, Services,ServiceDetails, Questions,Contact, Footer };
+// Import sign-in and sign-up components
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+
+export { Navbar, Home, About, Services, ServiceDetails, Questions, Contact, Footer, SignIn, SignUp };

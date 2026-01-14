00047f60de73af234517eb9040fccef3634188cd
Bari-Elwerfally <bariwer@gmail.com>
2026-01-14 14:48:35 +0200

    Update CSS and JavaScript files to remove references to the old title.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// Import existing components
+import Home from "./container/Home";
+import About from "./container/About";
+import Questions from "./container/Questions";
+import Services from "./container/Service/Services";
+import ServiceDetails from "./container/Service/ServiceDetails";
+import Contact from "./container/Contact";
+import Footer from "./container/Footer";
+import Navbar from "./Navbar/Navbar";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Import sign-in and sign-up components
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+
+export { Navbar, Home, About, Services, ServiceDetails, Questions, Contact, Footer, SignIn, SignUp };
+
+/* Remove or update references to old title */
